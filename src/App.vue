fafe35620f556fa77337020da23719b478b5e2b3
yoruwitch <evelyn.fernandes4@gmail.com>
2025-01-20 15:40:01 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+
+export default {
+    components: { Banner },
+};
+</script>
+
+<template>
+    <Banner />
+</template>
+
+<style scoped></style>
